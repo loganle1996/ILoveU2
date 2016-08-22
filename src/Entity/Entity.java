9f336a3eb9ac49a9f51a2c02d687d737b33a7173
Logan <loganle1996@gmail.com>
2016-08-22 13:24:48 +0700

    fixed AI

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -91,6 +91,15 @@ public abstract class Entity{
         return y;
     }
 
+    public int getNumberIceBall() {
+        return numberIceBall;
+    }
+
+    public void setNumberIceBall(int numberIceBall) {
+        this.numberIceBall = numberIceBall;
+    }
+
+    
     public void setY(int y) {
         this.y = y;
     }
@@ -298,11 +307,11 @@ public abstract class Entity{
     public void watchingAround(){
         
         if(facing == 0){
-            bigRectangle2D =  new Rectangle2D(this.getX()-200, this.getY()-200, 200, 400);
+            bigRectangle2D =  new Rectangle2D(this.getX()-200, this.getY()-200, 210, 400);
             smallRectangle2D = new Rectangle2D(this.getX()-100,this.getY(), 100, 40);
         }
         else if(facing == 1){
-            bigRectangle2D =  new Rectangle2D(this.getX(), this.getY()-200, 200, 400);
+            bigRectangle2D =  new Rectangle2D(this.getX()-10, this.getY()-200, 200, 400);
             smallRectangle2D = new Rectangle2D(this.getX(),this.getY(), 140, 40);
         }
         
